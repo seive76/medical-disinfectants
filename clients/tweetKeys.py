3702757dc55d1fd02180aded4a2351ac1c3ce1fa
jongbumlee <seive76@gmail.com>
2022-10-06 18:19:11 +0900

    make gitignore.

---
+++ clients/tweetKeys.py
@@ -0,0 +1,4 @@
+api_key = "vVG6WEYcbTdXvPmusUvpzH3sE"
+api_key_secret = "ChrmklHwJfeeWXKu1Wi5DHdHJF839QxaTA4zDvEk7HRffcuEcj"
+access_token = "1838120562-5rDNXh1mOoW2Zq8l2eUX4oAillgz1IcgkOGuiVt"
+access_secret = "64sRs49h7EtptXYS9fvMsmBZcdbOFUxX1WJZSLhqKTSBe"
