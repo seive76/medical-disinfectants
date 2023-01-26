8ac7837ce5af9f12e2373c78808c7dde98f32c2b
jongbumlee <seive76@gmail.com>
2023-01-26 10:59:22 +0900

    Update url and service key valid by 25/01/26.

--- disin_basic/disinfectants.py
+++ disin_basic/disinfectants.py
@@ -66,11 +66,14 @@ def parse():
         }
 
 # 공공데이터 API 접속 url & 기본정보 파라미터
-url = 'http://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService02/getDrugPrdtPrmsnInq02?'
+# 기존 url = 'http://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService02/getDrugPrdtPrmsnInq02?'
+url = 'http://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService03/getDrugPrdtPrmsnInq03?'
 
 # 공공데이터 service key
-service_key = 'i4kidk21c3X/hLkySwIiPJ8aTr1xRqQ/CWnluHr1zG2jx1LUBZrsfheuojj5ZlO79XhfL0gAEfmrz3TqpVWidg=='
+# 기존 service_key = 'i4kidk21c3X/hLkySwIiPJ8aTr1xRqQ/CWnluHr1zG2jx1LUBZrsfheuojj5ZlO79XhfL0gAEfmrz3TqpVWidg=='
 
+# 공공데이터 service key by 2025/01/26
+service_key ='i4kidk21c3X/hLkySwIiPJ8aTr1xRqQ/CWnluHr1zG2jx1LUBZrsfheuojj5ZlO79XhfL0gAEfmrz3TqpVWidg=='
 
 # 공공데이터 의약품 기본정보 파싱 함수
 # def disin_parsing():
